caa6f8f5d3395b323d0ad2722e3c9a7dd3e4296b
Jung Ji Yeon <jws1784@gmail.com>
2025-10-20 16:50:37 +0900

    Update 026_Remove-Duplicates-from-Sorted-Array.py

--- 01_array_string/026_Remove-Duplicates-from-Sorted-Array.py
+++ 01_array_string/026_Remove-Duplicates-from-Sorted-Array.py
@@ -1,14 +1,27 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         
-        k = 0
-        uniq_vals = []
+        # =================== #
+        # ====== Sol 1 ====== #
+        # =================== #
+        # k = 0
+        # uniq_vals = []
 
-        for i in range(len(nums)):
-            if nums[i] not in uniq_vals:
+        # for i in range(len(nums)):
+        #     if nums[i] not in uniq_vals:
+        #         nums[k] = nums[i]
+        #         k += 1
+        #         uniq_vals.append(nums[i])
+
+        # return k
+
+        # =================== #
+        # ====== Sol 2 ====== #
+        # =================== #
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
                 nums[k] = nums[i]
                 k += 1
-                uniq_vals.append(nums[i])
 
         return k
-
