fd5986d66dca9f64337dbb3616b426d086ac488a
Jung Ji Yeon <jws1784@gmail.com>
2025-11-03 19:41:34 +0900

    Update 45_Jump-Game-II.py

--- 01_array_string/45_Jump-Game-II.py
+++ 01_array_string/45_Jump-Game-II.py
@@ -5,6 +5,9 @@ class Solution:
         end = 0
         max_reach = 0
 
+        # end : 지금 한 번 점프해서 닿을 수 있는 범위
+        # max_reach : 다음에 점프할 때 가장 멀리 갈 수 있는 지점
+
         for i, jump in enumerate(nums[:-1]):
             max_reach = max(max_reach, i+jump)
 
