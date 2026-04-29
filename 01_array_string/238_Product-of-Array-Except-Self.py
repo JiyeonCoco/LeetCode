854b8134aacea74cd2f59c3083756600f71498dc
Jung Ji Yeon <jws1784@gmail.com>
2026-04-29 22:23:11 +0900

    Update 238_Product-of-Array-Except-Self.py

--- 01_array_string/238_Product-of-Array-Except-Self.py
+++ 01_array_string/238_Product-of-Array-Except-Self.py
@@ -1,18 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        left = [1] * len(nums)
+        right = [1] * len(nums)
 
-        # 왼쪽 순회 / 오른쪽 순회하면 손쉽게 구할 수 있음
-        n = len(nums)
-        res = [1] * n
-        
-        # 1️⃣ 왼쪽 곱 계산
-        for i in range(1, n):
-            res[i] = res[i-1] * nums[i-1]
-        
-        # 2️⃣ 오른쪽 곱 계산 및 합치기
-        right = 1
-        for i in reversed(range(n)):
-            res[i] *= right
-            right *= nums[i]
-        
-        return res
+        for idx in range(1, len(nums)):
+            left[idx] = left[idx-1] * nums[idx-1]
+
+        for idx in range(len(nums)-2, -1, -1):
+            right[idx] = right[idx+1] * nums[idx+1]
+
+        results = left.copy()
+        for idx in range(len(right)):
+            results[idx] *= right[idx]
+
+        return results
