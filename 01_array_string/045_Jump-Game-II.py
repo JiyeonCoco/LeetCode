af29d9dc32e26c03b4ea20b28163496f93401792
Jung Ji Yeon <jws1784@gmail.com>
2026-04-29 21:45:37 +0900

    Update 045_Jump-Game-II.py

--- 01_array_string/045_Jump-Game-II.py
+++ 01_array_string/045_Jump-Game-II.py
@@ -1,20 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-
-        jumps = 0
-        end = 0
-        max_reach = 0
+        
+        # Greedy 알고리즘
 
         # end : 지금 한 번 점프해서 닿을 수 있는 범위
         # max_reach : 다음에 점프할 때 가장 멀리 갈 수 있는 지점
+        end = 0
+        max_reach = 0
+        cnt = 0
 
-        for i, jump in enumerate(nums[:-1]):
-            max_reach = max(max_reach, i+jump)
+        for idx, val in enumerate(nums[:-1]):
+            max_reach = max(max_reach, idx+val)
 
-            if i == end:
-                jumps += 1
+            if idx == end:
+                cnt += 1
                 end = max_reach
 
-        return jumps
-
-        
+        return cnt
