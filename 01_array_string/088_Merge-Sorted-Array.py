bc1b0a002d397ebaa1f9ac8aaff8757e38b29f1a
Jung Ji Yeon <jws1784@gmail.com>
2026-04-29 18:21:28 +0900

    Update 088_Merge-Sorted-Array.py

--- 01_array_string/088_Merge-Sorted-Array.py
+++ 01_array_string/088_Merge-Sorted-Array.py
@@ -3,18 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
-        nums1_pos = m-1
-        nums2_pos = n-1
-        merge_pos = m+n-1
 
-        while nums1_pos >= 0 and nums2_pos >= 0:
-            if nums1[nums1_pos] > nums2[nums2_pos]:
-                nums1[merge_pos] = nums1[nums1_pos]
-                nums1_pos -= 1
+        n1_pos = m-1
+        n2_pos = n-1
+        add_pos = m+n-1
+
+        while n2_pos >= 0:
+            if n1_pos >= 0 and nums1[n1_pos] > nums2[n2_pos]:
+                nums1[add_pos] = nums1[n1_pos]
+                n1_pos -= 1
             else:
-                nums1[merge_pos] = nums2[nums2_pos]
-                nums2_pos -= 1
-            merge_pos -= 1
-
-        nums1[:nums2_pos + 1] = nums2[:nums2_pos + 1]
+                nums1[add_pos] = nums2[n2_pos]
+                n2_pos -= 1
+            add_pos -= 1
